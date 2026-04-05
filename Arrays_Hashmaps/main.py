05bbe82975df7dcf3424f129a2ffff7fb67b2df8
mattjoe <matthew.j.joe@gmail.com>
2026-04-04 22:15:42 -0400

    Valid Anagram problem

--- Arrays_Hashmaps/main.py
+++ Arrays_Hashmaps/main.py
@@ -1,17 +1,16 @@
 class Solution:
     
     # Problem 1
-    my_list = [1,2,3,4,4]
-
     # O(n2)
-    def algo1(my_list: list[int]) -> list[int]:
+
+    def algo1(self) -> list[int]:
+        my_list = [1, 2, 3, 4, 4]
         for pointer1 in range(len(my_list)):
             for pointer2 in range(pointer1+1, len(my_list)):
                 print(my_list[pointer1],my_list[pointer2])
                 if my_list[pointer1] == my_list[pointer2]:
                     print("Found em'")
-
-    # algo1()
+                    return [my_list[pointer1], my_list[pointer2]]
 
     # ------------------------------------------------------------------------------------------------------------
 
@@ -22,11 +21,10 @@ class Solution:
         # b = [2,5,1]
         # output = [2,5,0,0]
 
+    def calculate_array_sum(self):
+        list_a = [3, 5, 9]
+        list_b = [2, 5, 1]
 
-    list_a = [3,5,9]
-    list_b = [2,5,1]
-
-    def calculate_array_sum(self, list_a, list_b):
         output = []
         carry = 0
         i = len(list_a) - 1
@@ -64,6 +62,10 @@ class Solution:
     # Input: nums = [1, 2, 3, 4]
     # Output: false
 
+    # Explanation:
+    # Using a hashset is the optimal data structure because it has a O(1) lookup, vs an array O(n)
+    # Sorting the array would have been another solution but that is O(n log n), slower.
+
     def has_duplicates(self) -> bool:
         nums = [1, 2, 3, 3]
         hashset = set()
@@ -78,8 +80,51 @@ class Solution:
         print("No duplicates ")
         return False
 
-    # Using a hashset is the optimal data structure because it has a O(1) lookup, vs an array O(n)
-    # Sorting the array would have been another solution but that is O(n log n), slower.
+    # ------------------------------------------------------------------------------------------------------------
+
+    # Problem 4: Valid Anagram
+    # Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+    # An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+    # Examples:
+        # Input: s = "racecar", t = "carrace"
+        # Output: true
+
+        #Input: s = "jar", t = "jam"
+        # Output: false
+
+    # Explanation:
+    # hashmap: {"r":2, "a": 2} adding one string to a map ( O(n) for insert and O(1) for lookup )
+    # return False early if the two strings have different lengths
+    # loop through the first string and update the map with the number of instances of the character ( O(n) for the iteration )
+    # loop through the second string and check:
+        # is the char in the map ? decrement : return False
+    # if we get through that second loop, return True
+    # overall O(n)
+
+    def isAnagram(self):
+        s = "racecar"
+        t = "carrace"
+
+        map = {}
+
+        if len(s) != len(t):
+            return False
+
+        for s_chars in s:
+           if s_chars in map:
+               map[s_chars] += 1
+           else:
+               map[s_chars] = 1
+
+        for t_chars in t:
+            if t_chars in map:
+                map[t_chars] -= 1
+            else:
+                return False
+
+        return True
+
 
 
 solution = Solution()
